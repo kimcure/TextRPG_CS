5c14b306bfcd1d2a08e34ce3d1a5029de6864904
kimcure <dellwons@gmail.com>
2025-04-30 20:19:57 +0900

    25.4.30//Charcter default1

---
+++ TextRPG_C#/Character.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Character
+{
+    public string Name { get; set; }
+    public int Health { get; set; }
+    public int Defense { get; set; }
+    public int AttackPower { get; set; }
+
+    public Character(string name, int health, int defense, int attackpower)
+    {
+        Name = name;
+        Health = health;
+        Defense = defense;
+        AttackPower = attackpower;
+    }
+
+    public void Attack(Character target)
+    {
+        Console.WriteLine($"{Name}이/가 {target.Name}을/를 공격합니다!");
+        target.TakeDamage(AttackPower);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        damage -= Defense;
+        Health -= damage;
+
+        Console.WriteLine(Name + "의 남은 체력: " + Health);
+
+        if (Health < 0)
+        {
+            Health = 0;
+        }
+    }
+}
--- TextRPG_C#/Program.cs
+++ TextRPG_C#/Program.cs
@@ -4,6 +4,10 @@ public class Program
 {
     static void Main()
     {
+        Character player = new Character("Name", 100, 10, 20);
+        Character enemy = new Character("Name2", 100, 15, 15);
 
+        player.Attack(enemy);
+        enemy.Attack(player);
     }
 }
